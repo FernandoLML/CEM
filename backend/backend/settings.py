e9e7cdefd91c56e2b2f6085aa2805dda978d7d18
FernandoLML <fernandolucasmoraesdaluz@gmail.com>
2025-05-12 10:31:41 -0300

    Back - Conexão com o banco de dados estabelecida

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,3 +1,5 @@
+from decouple import config
+
 """
 Django settings for backend project.
 
@@ -74,8 +76,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
+        'OPTIONS': {
+            'sslmode': 'require',  # SSL obrigatório na conexão com Supabase
+        }
     }
 }
 
